1b6e04bf8f97e719ce767b67f364e2e690a19689
Louise Poubel <louise@openrobotics.org>
2022-04-25 09:29:12 -0700

    Rebase from 'upstream'

--- ros_ign_gazebo/src/create.cpp
+++ ros_ign_gazebo/src/create.cpp
@@ -17,11 +17,13 @@
 #include <ignition/msgs/entity_factory.pb.h>
 #include <ignition/msgs/Utility.hh>
 #include <ignition/transport/Node.hh>
-#include <rclcpp/rclcpp.hpp>
-#include <std_msgs/msg/string.hpp>
 
 #include <sstream>
 #include <string>
+
+#include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/string.hpp>
+
 
 DEFINE_string(world, "", "World name.");
 DEFINE_string(file, "", "Load XML from a file.");
@@ -47,9 +49,9 @@ int main(int _argc, char ** _argv)
 
   gflags::AllowCommandLineReparsing();
   gflags::SetUsageMessage(
-    R"(Usage: create -world [arg] [-file FILE] [-param PARAM] [-topic TOPIC]
-                       [-string STRING] [-name NAME] [-X X] [-Y Y] [-Z Z]
-                       [-Roll ROLL] [-Pitch PITCH] [-Yaw YAW])");
+    R"(Usage: create -world [arg] [-file FILE] [-param PARAM] [-topic TOPIC] 
+                       [-string STRING] [-name NAME] [-allow_renaming RENAMING] [-x X] [-y Y] [-z Z]
+                       [-R ROLL] [-P PITCH] [-Y YAW])");
   gflags::ParseCommandLineFlags(&_argc, &_argv, true);
 
   // World
@@ -91,7 +93,7 @@ int main(int _argc, char ** _argv)
   if (!FLAGS_file.empty()) {
     req.set_sdf_filename(FLAGS_file);
   } else if (!FLAGS_param.empty()) {  // Param
-    ros2_node->declare_parameter(FLAGS_param);
+    ros2_node->declare_parameter<std::string>(FLAGS_param);
 
     std::string xmlStr;
     if (ros2_node->get_parameter(FLAGS_param, xmlStr)) {
