b23cc78d5a6a3e9e48e22d90f4a0cd2e68e4a96a
Michael Carroll <mjcarroll@intrinsic.ai>
2023-05-18 12:45:52 +0000

    Rebase from 'debian/iron/ros_ign_gazebo_demos'

--- launch/gpu_lidar_bridge.launch.py
+++ launch/gpu_lidar_bridge.launch.py
@@ -1,4 +1,4 @@
-# Copyright 2019 Open Source Robotics Foundation, Inc.
+# Copyright 2022 Open Source Robotics Foundation, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,49 +17,18 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
 from launch.actions import IncludeLaunchDescription
-from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
-
-from launch_ros.actions import Node
 
 
 def generate_launch_description():
+    print('ros_ign_gazebo_demos is deprecated! Please use ros_gz_sim_demos instead!')
 
-    pkg_ros_ign_gazebo_demos = get_package_share_directory('ros_ign_gazebo_demos')
-    pkg_ros_ign_gazebo = get_package_share_directory('ros_ign_gazebo')
-
-    ign_gazebo = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(pkg_ros_ign_gazebo, 'launch', 'ign_gazebo.launch.py')),
-        launch_arguments={
-            'ign_args': '-r gpu_lidar_sensor.sdf'
-        }.items(),
-    )
-
-    # RViz
-    rviz = Node(
-       package='rviz2',
-       executable='rviz2',
-       arguments=['-d', os.path.join(pkg_ros_ign_gazebo_demos, 'rviz', 'gpu_lidar_bridge.rviz')],
-       condition=IfCondition(LaunchConfiguration('rviz'))
-    )
-
-    # Bridge
-    bridge = Node(
-        package='ros_ign_bridge',
-        executable='parameter_bridge',
-        arguments=['lidar@sensor_msgs/msg/LaserScan@ignition.msgs.LaserScan',
-                   '/lidar/points@sensor_msgs/msg/PointCloud2@ignition.msgs.PointCloudPacked'],
-        output='screen'
-    )
+    pkg_ros_gz_sim_demos = get_package_share_directory('ros_gz_sim_demos')
 
     return LaunchDescription([
-        ign_gazebo,
-        DeclareLaunchArgument('rviz', default_value='true',
-                              description='Open RViz.'),
-        bridge,
-        rviz,
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(pkg_ros_gz_sim_demos, 'launch', 'gpu_lidar_bridge.launch.py')),
+        )
     ])
