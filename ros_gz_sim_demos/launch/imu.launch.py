de24085ebf1a185b7a13cfd8e6f612f58b05a331
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2025-06-12 13:40:43 +0200

    Imported upstream version '1.0.15' of 'upstream'

--- ros_gz_sim_demos/launch/imu.launch.py
+++ ros_gz_sim_demos/launch/imu.launch.py
@@ -30,40 +30,49 @@ def generate_launch_description():
 
     pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
 
-    # RQt
-    rqt = Node(
-        package='rqt_plot',
-        executable='rqt_plot',
-        # FIXME: Why isn't the topic being populated on the UI? RQt issue?
-        arguments=['--force-discover',
-                   '/model/vehicle_blue/battery/linear_battery/state/percentage'],
-        condition=IfCondition(LaunchConfiguration('rqt'))
-    )
-
     gz_sim = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py')),
         launch_arguments={
-            'gz_args': '-r -z 1000000 linear_battery_demo.sdf'
+            'gz_args': '-r sensors.sdf'
         }.items(),
     )
+
+    # RQt
+    rqt = Node(
+        package='rqt_topic',
+        executable='rqt_topic',
+        arguments=['-t'],
+        condition=IfCondition(LaunchConfiguration('rqt'))
+    )
+
+    # RViz
+    # FIXME: Add once there's an IMU display for RViz2
+    # pkg_ros_gz_sim_demos = get_package_share_directory('ros_gz_sim_demos')
+    # rviz = Node(
+    #     package='rviz2',
+    #     executable='rviz2',
+    #     # arguments=['-d', os.path.join(pkg_ros_gz_sim_demos, 'rviz', 'imu.rviz')],
+    #     condition=IfCondition(LaunchConfiguration('rviz'))
+    # )
 
     # Bridge
     bridge = Node(
         package='ros_gz_bridge',
         executable='parameter_bridge',
-        arguments=[
-            '/model/vehicle_blue/cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist',
-            '/model/vehicle_blue/battery/linear_battery/state@sensor_msgs/msg/BatteryState@'
-            'gz.msgs.BatteryState'
-        ],
+        arguments=['/imu@sensor_msgs/msg/Imu@gz.msgs.IMU'],
         output='screen'
     )
 
     return LaunchDescription([
         gz_sim,
-        DeclareLaunchArgument('rqt', default_value='true',
-                              description='Open RQt.'),
+        DeclareLaunchArgument(
+            'rqt', default_value='true', description='Open RQt.'
+        ),
+        DeclareLaunchArgument(
+            'rviz', default_value='true', description='Open RViz.'
+        ),
         bridge,
         rqt
+        # rviz
     ])
--- ros_gz_sim_demos/launch/rgbd_camera_bridge.launch.py
+++ ros_gz_sim_demos/launch/rgbd_camera_bridge.launch.py
@@ -28,42 +28,49 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
 
+    pkg_ros_gz_sim_demos = get_package_share_directory('ros_gz_sim_demos')
     pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
-
-    # RQt
-    rqt = Node(
-        package='rqt_plot',
-        executable='rqt_plot',
-        # FIXME: Why isn't the topic being populated on the UI? RQt issue?
-        arguments=['--force-discover',
-                   '/model/vehicle_blue/battery/linear_battery/state/percentage'],
-        condition=IfCondition(LaunchConfiguration('rqt'))
-    )
 
     gz_sim = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py')),
         launch_arguments={
-            'gz_args': '-r -z 1000000 linear_battery_demo.sdf'
+            'gz_args': '-r sensors_demo.sdf'
         }.items(),
     )
 
+    # RViz
+    rviz = Node(
+        package='rviz2',
+        executable='rviz2',
+        arguments=[
+            '-d', os.path.join(pkg_ros_gz_sim_demos, 'rviz', 'rgbd_camera_bridge.rviz')
+        ],
+        condition=IfCondition(LaunchConfiguration('rviz'))
+    )
+
+    remappings = [('/camera', '/camera/image'),
+                  ('/camera_info', '/camera/camera_info')]
     # Bridge
     bridge = Node(
         package='ros_gz_bridge',
         executable='parameter_bridge',
         arguments=[
-            '/model/vehicle_blue/cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist',
-            '/model/vehicle_blue/battery/linear_battery/state@sensor_msgs/msg/BatteryState@'
-            'gz.msgs.BatteryState'
+            '/camera@sensor_msgs/msg/Image@gz.msgs.Image',
+            '/camera_info@sensor_msgs/msg/CameraInfo@gz.msgs.CameraInfo',
+            '/rgbd_camera/image@sensor_msgs/msg/Image@gz.msgs.Image',
+            '/rgbd_camera/camera_info@sensor_msgs/msg/CameraInfo@gz.msgs.CameraInfo',
+            '/rgbd_camera/depth_image@sensor_msgs/msg/Image@gz.msgs.Image',
+            '/rgbd_camera/points@sensor_msgs/msg/PointCloud2@gz.msgs.PointCloudPacked'
         ],
-        output='screen'
+        output='screen',
+        remappings=remappings,
     )
 
     return LaunchDescription([
         gz_sim,
-        DeclareLaunchArgument('rqt', default_value='true',
-                              description='Open RQt.'),
+        DeclareLaunchArgument('rviz', default_value='true',
+                              description='Open RViz.'),
         bridge,
-        rqt
+        rviz,
     ])
